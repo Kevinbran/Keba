19d2523760d7dcc8ac57f90ff28170feb0433f6a
BlackAlienCode <richard.bln@outlook.de>
2017-03-03 15:23:42 +0100

    AlienCode

--- framework.js
+++ framework.js
@@ -1,22 +1,3 @@
-(function(f) {
-    if (typeof exports === "object" && typeof module !== "undefined") {
-        module.exports = f();
-    } else if (typeof define === "function" && define.amd) {
-        define([], f);
-    } else {
-        var g;
-        if (typeof window !== "undefined") {
-            g = window;
-        } else if (typeof global !== "undefined") {
-            g = global;
-        } else if (typeof self !== "undefined") {
-            g = self;
-        } else {
-            g = this;
-        }
-        g.io = f();
-    }
-});
 
 // Extra    
 var head = document.head || document.getElementsByTagName('head')[0],
